5c38ab649c0c1e87e6d4950c087b21231a9055ec
Alex Wolski <alex.theinfinite@gmail.com>
2019-05-11 10:39:46 -0700

    Added Bundle ID

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { AppContainer } from './src/navigation/Router';
-export default class App extends React.Component {
-  
+
+export default class App extends React.Component {  
   render() {
     return (
       <AppContainer/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
